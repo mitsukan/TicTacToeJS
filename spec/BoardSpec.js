be072b0c16ec464f76dc1036e765120a879c8321
Jihin Ip <jim_ip20@hotmail.com>
2018-06-28 18:33:33 +0100

    First test pass, board has a grid

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -7,7 +7,7 @@ describe("Board", function() {
   describe("Board functions", function() {
 
     it("Should have 3 subarrays in an array to represent a grid", function() {
-      expect(board.grid).toBe([["","",""],["","",""],["","",""]]);
+      expect(board.grid).toEqual([["","",""],["","",""],["","",""]]);
     });
 
   });
