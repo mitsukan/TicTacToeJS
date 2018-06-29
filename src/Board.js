bd9465157a395ab5c870a18126ae56a25d9d1861
Jihin Ip <jim_ip20@hotmail.com>
2018-06-29 12:08:29 +0100

    Removing player X and O functions from Board

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,3 @@
 Board = function(){
   this.grid = [["","",""],["","",""],["","",""]]
 };
-
-Board.prototype.playerX = function(array, subarray){
-  this.grid[array][subarray] = "X"
-};
-
-Board.prototype.playerO = function(array, subarray){
-  this.grid[array][subarray] = "O"
-};
