bea66ea846e3a3ef0b6dd50a12a9de8e9115c1d9
Jihin Ip <jim_ip20@hotmail.com>
2018-06-29 11:30:43 +0100

    Adding and passing a test for playerO, amending playerX test name

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -12,9 +12,14 @@ describe("Board", function() {
       expect(board.grid).toEqual([["","",""],["","",""],["","",""]]);
     });
 
-    it("Change empty box to X", function() {
+    it("PlayerX can add a X", function() {
       board.playerX(1,1)
       expect(board.grid).toEqual([["","",""],["","X",""],["","",""]]);
+    });
+
+    it("PlayerO can add an O", function() {
+      board.playerO(1,1)
+      expect(board.grid).toEqual([["","",""],["","O",""],["","",""]]);
     });
 
   });
--- src/Board.js
+++ src/Board.js
@@ -5,3 +5,7 @@ Board = function(){
 Board.prototype.playerX = function(array, subarray){
   this.grid[array][subarray] = "X"
 };
+
+Board.prototype.playerO = function(array, subarray){
+  this.grid[array][subarray] = "O"
+};
