91fa97cf31c14d7db7682a0436add601cb7c5a66
Jihin Ip <jim_ip20@hotmail.com>
2018-06-29 11:54:52 +0100

    Deleting player functionality tests from Board

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -12,16 +12,6 @@ describe("Board", function() {
       expect(board.grid).toEqual([["","",""],["","",""],["","",""]]);
     });
 
-    // it("PlayerX can add a X", function() {
-    //   board.playerX(1,1)
-    //   expect(board.grid).toEqual([["","",""],["","X",""],["","",""]]);
-    // });
-    //
-    // it("PlayerO can add an O", function() {
-    //   board.playerO(1,1)
-    //   expect(board.grid).toEqual([["","",""],["","O",""],["","",""]]);
-    // });
-
   });
 
 });
